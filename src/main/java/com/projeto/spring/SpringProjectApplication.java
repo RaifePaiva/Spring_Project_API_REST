3bed623f9c3be45a298f9205b7e12cf370aaf22f
RaifePaiva <raife.tec@gmail.com>
2020-04-05 01:54:02 -0300

    Criando operacao de instanciacao (Mock de dados para testar no banco h2)

--- src/main/java/com/projeto/spring/SpringProjectApplication.java
+++ src/main/java/com/projeto/spring/SpringProjectApplication.java
@@ -1,13 +1,36 @@
 package com.projeto.spring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.projeto.spring.domain.Categoria;
+import com.projeto.spring.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SpringProjectApplication {
+public class SpringProjectApplication implements CommandLineRunner {
+	
+	// dependencia para mockar os dados.
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	////////
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringProjectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// Intanciando objetos para testes (Mockando).
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
