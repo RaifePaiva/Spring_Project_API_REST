2768fcca3bcc06b60ed4e00cb76334f5ddf2a3da
RaifePaiva <raife.tec@gmail.com>
2020-05-11 11:28:43 -0300

    Email HTML

--- src/main/java/com/projeto/spring/services/EmailService.java
+++ src/main/java/com/projeto/spring/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.projeto.spring.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.projeto.spring.domain.Pedido;
@@ -9,6 +11,10 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);	
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 	
 
 }
--- src/main/java/com/projeto/spring/services/MockEmailService.java
+++ src/main/java/com/projeto/spring/services/MockEmailService.java
@@ -1,8 +1,12 @@
 package com.projeto.spring.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
+
+import com.projeto.spring.domain.Pedido;
 
 public class MockEmailService extends AbstractEmailService {
 
@@ -13,6 +17,22 @@ public class MockEmailService extends AbstractEmailService {
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Simulando envio de email....");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado");
+		
+	}
+
+
+	@Override
+	public void sendOrderConfirmationHtmlEmail(Pedido obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de email HTML....");
 		LOG.info(msg.toString());
 		LOG.info("Email enviado");
 		
