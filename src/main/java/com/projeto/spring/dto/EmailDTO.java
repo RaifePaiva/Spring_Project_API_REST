9e3ad4bf36b5f99d6591dd559bd2b7228a7fcc1b
RaifePaiva <raife.tec@gmail.com>
2020-05-18 04:01:13 -0300

    Esqueci a senha

---
+++ src/main/java/com/projeto/spring/dto/EmailDTO.java
@@ -0,0 +1,22 @@
+package com.projeto.spring.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+public class EmailDTO {
+	
+	@NotEmpty(message = "preenchiimento obrigatório!")
+	@Email(message = "E-mail inválido")
+	private String email;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
--- src/main/java/com/projeto/spring/resources/AuthResource.java
+++ src/main/java/com/projeto/spring/resources/AuthResource.java
@@ -1,20 +1,27 @@
 package com.projeto.spring.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.projeto.spring.dto.EmailDTO;
 import com.projeto.spring.security.JWTUtil;
 import com.projeto.spring.security.UserSS;
+import com.projeto.spring.services.AuthService;
 import com.projeto.spring.services.UserService;
 
 public class AuthResource {
 	
 	@Autowired
 	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private AuthService service;
 
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
@@ -23,5 +30,15 @@ public class AuthResource {
 		response.addHeader("Authorization", "Bearer " + token);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+		service.sendNewPassword(objDto.getEmail());
+		return ResponseEntity.noContent().build();
+	}
+	
+	
+	
+	
 
 }
